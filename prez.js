9958b02c9cf2dbae0784106dd4b1d993b8b660ca
drtdenis <drouetdenis2@gmail.com>
2021-02-12 12:41:15 +0100

    add exos -intro

---
+++ prez.js
@@ -0,0 +1,5 @@
+let name = 'Sofiane';
+let age = 39;
+let ville = 'Paris';
+
+console.log('Hello, my name is ' + name + ' and i am ' + age + ' years old. I live in ' + ville + '.')
---
+++ pyramid_generic.js
@@ -0,0 +1,10 @@
+let any = 'A'
+
+console.log(`${any}              ${any}`)
+console.log(`${any}${any}            ${any}${any}`)
+console.log(`${any}${any}${any}          ${any}${any}${any}`)
+console.log(`${any}${any}${any}${any}        ${any}${any}${any}${any}`)
+console.log(`${any}${any}${any}${any}${any}      ${any}${any}${any}${any}${any}`)
+console.log(`${any}${any}${any}${any}${any}${any}    ${any}${any}${any}${any}${any}${any}`)
+console.log(`${any}${any}${any}${any}${any}${any}${any}  ${any}${any}${any}${any}${any}${any}${any}`)
+console.log(`${any}${any}${any}${any}${any}${any}${any}${any}${any}${any}${any}${any}${any}${any}${any}${any}`)
